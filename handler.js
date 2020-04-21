9a9d58b494f23a6a2fa20c506763925ff59f8b7e
Sam Kavanagh <hganavak@gmail.com>
2020-04-21 16:27:45 +1200

    Added URL param support

--- handler.js
+++ handler.js
@@ -2,6 +2,22 @@
 
 module.exports.hello = async event => {
 
+  // If people have passed a name URL param
+  if(event.queryStringParameters && event.queryStringParameters.name) {
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: 'Sup ' + event.queryStringParameters.name + ', nice to meet you!',
+        },
+        null,
+        2
+      ),
+    };
+  }
+
+
   // Use this code if you want to use the LAMBDA-PROXY integration
   // This allows you to control API-Gateway within your Lambda functionk
   return {
@@ -9,7 +25,7 @@ module.exports.hello = async event => {
     body: JSON.stringify(
       {
         message: 'Go Serverless v1.0! Sam was here.',
-        input: event,
+        input: event, // Shows the event that the Lambda function received (headers, resource, path, method, pathParameters, stageVariables)
       },
       null,
       2
